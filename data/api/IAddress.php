c51c8c08a19fbf1c1db5502c1520526100dbc1bf
ccyznl <2845843140@qq.com>
2018-05-08 23:04:44 +0800

    声明地址接口

--- data/api/IAddress.php
+++ data/api/IAddress.php
@@ -10,4 +10,14 @@ namespace data\api;
 
 interface IAddress {
 
+    /**
+     * 获取区域列表
+     */
+    function getAreaList();
+
+    /**
+     * 获取省列表
+     * @param number $area_id
+     */
+    function getProvinceList($area_id = 0);
 }
